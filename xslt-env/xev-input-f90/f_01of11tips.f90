bdfb7ed4b5d1ab70d6e597fe3959be299e2351a7
ishimori <ishimori@hagi-a>
2014-02-24 09:22:18 +0900

    fortran Tip #1 作成

---
+++ xslt-env/xev-input-f90/f_01of11tips.f90
@@ -0,0 +1,20 @@
+
+
+SUBROUTINE TEST01( M,N )
+    INTEGER, DIMENSION(10) :: tmp
+    INTEGER, DIMENSION(M,N) :: A
+
+    !$acc kernels loop
+    DO i = 1, M
+    !$xev dir add( acc, loop, private(tmp） )
+        DO j = 1, N
+            DO jj = 1, 10
+                tmp(jj) = jj
+            END DO
+            A(i,j) = SUM(tmp)
+        END DO
+    END DO
+
+    RETURN
+END
+
