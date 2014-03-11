d8c11dc23396bd94ce78e958e3d5051eb77f5f8c
hirasawa <hirasawa@hagi-a>
2014-03-11 11:48:45 +0900

    catalog-no14-1.f90

---
+++ xslt-env/xev-input-f90/catalog-no14-1.f90
@@ -0,0 +1,17 @@
+
+program catalog_no14
+
+  integer,parameter :: n=139
+
+  real*8 A(n, n, n), B(n, n, n)
+
+  do k=2,n-1
+     do j=2,n-1
+        do i=2,n-1
+           B(i,j,k) = A(i,j,k)
+        end do
+     end do
+  end do
+
+end program catalog_no14
+
