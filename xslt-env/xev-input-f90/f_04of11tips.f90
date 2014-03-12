6f443e39a3a5981ab1bf696594d9a3d214e708f3
ishimori <ishimori@hagi-a>
2014-03-12 17:35:59 +0900

    範囲指定対応

--- xslt-env/xev-input-f90/f_04of11tips.f90
+++ xslt-env/xev-input-f90/f_04of11tips.f90
@@ -1,21 +1,29 @@
 
 SUBROUTINE TEST04( M,N )
+!$xev array1to2 type(A,X,Y)
+!$xev array1to2 varref(A,ii,jj)
     INTEGER  idx
     INTEGER A(M*N)
     INTEGER, DIMENSION(M,N) :: B
 
-    !$xev dir replace(acc, kernels, loop)
+!$xev array1to2 varref(C,iii,jjj,default)
     !$acc kernels loop
 
     do i = 1, M
+!$xev array1to2 varref(A,i,j)
+        A(idx) = B(i,j)
         do j = 1, N
             !$xev statement-del ptn-001
             idx = ((i-1)*M)+j
 
-            !$xev statement-rep ptn-001(A(i,j)=B(i,j))
+!$xev array1to2 varref(A,x,y)
             A(idx) = B(i,j)
         end do
+!$xev array1to2 varref(A,x,y,end)
+            A(idx) = 100
     end do
+!$xev array1to2 varref(A,100,300)
+            A(idx) = 200
 
     RETURN
 END
