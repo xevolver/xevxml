2225ea706609950fb2e1c559f5be4b4d51563c0b
author hirasawa@sh-Pro7500 1400552058 +0900 <author hirasawa@sh-Pro7500 1400552058 +0900>
2014-05-20 11:14:18 +0900

    Init: xslt-env/xev-translate/loop-interchange-multi.xsl

---
+++ xslt-env/xev-input-f90/loop-interchange-multi.f90
@@ -0,0 +1,54 @@
+
+SUBROUTINE SAMPLE_INTERCHANGE
+  
+    !$xev loop interchange(1)
+    DO I = 1, 15
+        DO J = 2, 25
+            SUM = SUM + 5
+        END DO
+    END DO
+  
+  
+    !$xev loop interchange(2)
+    DO I = 1, 15
+        DO J = 2, 25
+            DO K = 3, 35
+                SUM = SUM + 6
+            END DO
+        END DO
+    END DO
+  
+    DO I = 1, 15
+        !$xev loop interchange(1)
+        DO J = 2, 25
+            DO K = 3, 35
+                SUM = SUM + 7
+            END DO
+        END DO
+    END DO
+
+    DO I = 1, 15
+        !$xev loop interchange(2)
+        DO J = 2, 25
+            DO K = 3, 35
+                DO L = 4, 45
+                    SUM = SUM + 8
+                END DO
+            END DO
+        END DO
+    END DO
+
+    !$xev loop interchange(3)
+    DO I = 1, 15
+        DO J = 2, 25
+            DO K = 3, 35
+                DO L = 4, 45
+                    SUM = SUM + 9
+                END DO
+            END DO
+        END DO
+    END DO
+
+  
+    RETURN
+END SUBROUTINE SAMPLE_INTERCHANGE
