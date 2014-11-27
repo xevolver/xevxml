540449e6903df48fd14fa70d61173b46d885c90f
hirasawa@MX3 <>
2014-11-27 22:46:51 +0900

    test/triple-loop-1.f90.xev.d/test-loop_collapse/triple-loop-1.target.f90

--- test/triple-loop-1.f90.xev.d/test-loop_collapse/triple-loop-1.target.f90
+++ test/triple-loop-1.f90.xev.d/test-loop_collapse/triple-loop-1.target.f90
@@ -5,12 +5,9 @@ program triple_loop_1
 
     real*8 A(n, n, n), B(n, n, n)
 
-    !$xev loop_tag
-    do k=1,n-1
-        do j=1,n-1
-            do i=1,n-1
-                B(i,j,k) = A(i,j,k)
-            end do
+    do j=1,n-1
+        do i=1,(n-1)*(n-1)
+            B(i,j,1) = A(i,j,1)
         end do
     end do
 
