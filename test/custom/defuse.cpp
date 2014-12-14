601cb92037c8223ba23cce0826e20a6b246445b2
Hiroyuki Takizawa <tacky@isc.tohoku.ac.jp>
2014-12-14 16:56:13 +0900

    Add a sample of visitor class customization

---
+++ test/custom/defuse.cpp
@@ -0,0 +1,58 @@
+#include "rose.h"
+#include "DefUseAnalysis.h"
+#include <string>
+#include <iostream>
+#include <xevxml.hpp>
+
+using namespace std;
+
+class DefuseUser:public XevXml::XevSageVisitor
+{
+  DFAnalysis* defuse;
+public:
+  DefuseUser():XevSageVisitor() {
+    defuse = NULL;
+  }
+
+  bool defuseAnalysis(SgProject* p){
+    // Call the Def-Use Analysis
+    bool debug = false;
+    defuse = new DefUseAnalysis(p);
+    defuse->run(debug);
+  }
+
+  /* add a new attribute to SgVarRefExp nodes */
+  void attribSgVarRefExp(SgNode* node){
+    /* write default attributes */
+    XevSageVisitor::attribSgVarRefExp(node);
+
+    /* write an additional attribute */
+    SgVarRefExp * varRef = isSgVarRefExp(node);
+    SgInitializedName* initName
+      = isSgInitializedName(varRef->get_symbol()->get_declaration());
+    vector<SgNode* > vec = defuse->getDefFor(varRef, initName);
+    sstr() << " num_defs=\"" << vec.size() <<"\" ";
+  }
+};
+
+int main( int argc, char * argv[] )
+{
+  vector<string> argvList(argv, argv + argc);
+  SgProject* project = frontend(argvList);
+
+  XevXml::XevInitialize();
+
+  /* setup an AST visitor class object */
+  DefuseUser visitor;
+  XevXml::XevXmlOption opt;
+  visitor.setXmlOption(&opt);
+
+  /* do Def-Use analysis */
+  visitor.defuseAnalysis(project);
+
+  /* visit every node and write it as an XML element */
+  visitor.write(cout,&project);
+
+  XevXml::XevFinalize();
+  return 0;
+}
---
+++ test/custom/input_defuse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int x = 9;
+  if(x>0)
+    x = x + 1;
+  else
+    x = x + 2;
+  printf("%d\n",x);
+}
