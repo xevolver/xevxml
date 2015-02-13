91681c7f25482f0097ec1835a12b6456dbb2c603
hirasawa@MX3 <hirasawa@computer.org>
2015-02-13 09:51:25 +0900

    cp from Resource-Undo-aq

--- test/triple-loop-2.f90
+++ test/triple-loop-2.f90
@@ -1,18 +1,34 @@
 
-program triple_loop_1
-
-    integer,parameter :: n=139
-
-    real*8 A(n, n, n), B(n, n, n)
-
-    !$xev loop_tag
-    do k=1,n-1
-        do j=1,n-1
-            do i=1,n-1
-                B(i,j,k) = A(i,j,k)
-            end do
+program TileLoop
+    implicit none
+    integer:: i, j, i1, j1,k
+    integer, parameter :: N = 3500
+    real:: a(N,N), b(N,N), c(N,N)
+    real :: t1,t2,ta
+    do j=1,N
+        do i=1,N
+            a(i,j)=i+j
+            b(i,j)=i*j
+            c(i,j)=0.0
         end do
     end do
 
-end program  triple_loop_1
+    call cpu_time(t1)
+    do k=1,100
+        do j1=1,floor(real(N-1)/1)*1+1,1
+            do i1=1,floor(real(N-1)/1)*1+1,1
+                do j=max(1,j1),min(N,j1+0)
+                    do i=max(1,i1),min(N,i1+0)
+                        a(i,j)=a(i,j)+b(i,j)*a(i,j)
+                    end do
+                end do
+            end do
+        end do
+    end do
+    call cpu_time(t2)
+    ta=real(t2-t1)/100
+    print *,  ta
+
+end program TileLoop
+
 
