289f96e0a56b20f49862e9b511e9cd6730b470b7
Hiroyuki Takizawa <tacky@isc.tohoku.ac.jp>
2015-03-11 00:53:53 +0900

    Remove an unecessay file, comment_preprocessed.f (automatically generated)

--- test/unparse/input/comment_postprocessed.f
+++
@@ -1,6 +0,0 @@
-# 1 "/home/tacky/src/xevxml-public/test/unparse/input/comme5LDv7e.F90"
-# 1 "<command-line>"
-# 1 "/home/tacky/src/xevxml-public/test/unparse/input/comme5LDv7e.F90"
-c23456     
-      real, dimension(10) :: a
-      end
