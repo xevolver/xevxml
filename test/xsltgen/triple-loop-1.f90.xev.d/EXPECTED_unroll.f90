61a1975749eea626cc6b48f7d8d9640aecd613d9
Hiroyuki Takizawa <takizawa@cc.tohoku.ac.jp>
2015-05-08 02:37:38 +0900

    Add test/xsltgen for testing the xsltgen command

---
+++ test/xsltgen/triple-loop-1.f90.xev.d/EXPECTED_unroll.f90
@@ -0,0 +1,14 @@
+PROGRAM triple_loop_1
+INTEGER, PARAMETER :: n = 139
+REAL(kind=8) :: A(n,n,n), B(n,n,n)
+!pragma 
+    !$xev loop_tag
+DO k = 1, n - 1
+DO j = 1, n - 1
+DO i = 1, n - 1
+B(i,j,k) = A(i,j,k)
+END DO
+END DO
+END DO
+END PROGRAM triple_loop_1
+
